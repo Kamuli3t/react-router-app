4495a7ec54b8616c40eecc599ef1abaf199c58fb
kamuli3t <kamuli3t@gmail.com>
2025-03-09 22:25:21 -0700

    update Main component to display cryptocurrency information and change Price API URL to secure HTTPS

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <h1>This is a Crypto Currency!</h1>
+      <h2>with the help of coinApi, it provides the rate to USD.</h2>
+    </>
+  );
 }
 
 export default Main;
--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -8,7 +8,7 @@ export default function Price() {
   const params = useParams();
   const symbol = params.symbol;
   // Using the other two variables to create our URL.
-  const url = `http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
+  const url = `https://rest.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`;
 
   // State to hold the coin data.
   const [coin, setCoin] = useState("null");
